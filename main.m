66d1d92415d16489c9dac66c3e706cb2c5dc7ced
Eduardo Costa <m4c0@users.noreply.github.com>
2026-06-06 16:44:45 +0100

    "new" way to create windows on iOS 26

--- main.m
+++ main.m
@@ -1,20 +1,42 @@
 #import <CoreFoundation/CoreFoundation.h>
 #import <UIKit/UIKit.h>
 
-@interface POCAppDelegate : NSObject<UIApplicationDelegate>
-@property(nonatomic, strong) UIWindow * window;
+@interface POCWindowSceneDelegate : NSObject<UIWindowSceneDelegate>
+@property (nonatomic, strong) UIWindow * window;
 @end
-@implementation POCAppDelegate
-- (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(id)options {
+@implementation POCWindowSceneDelegate
+- (void) scene:(UIScene *) scene willConnectToSession:(UISceneSession *) session options:(UISceneConnectionOptions *) connectionOptions
+{
+  UIWindowScene * windowScene = (UIWindowScene *)scene;
+
   UIViewController * vc = [UIViewController new];
   vc.view = [UIView new];
+  vc.view.backgroundColor = [UIColor redColor];
 
-  self.window = [UIWindow new];
-  self.window.frame = [UIScreen mainScreen].bounds;
-  self.window.rootViewController = vc;
+  UIWindow * w = [[UIWindow alloc] initWithWindowScene:windowScene];
+  w.rootViewController = vc;
+
+  self.window = w;
   [self.window makeKeyAndVisible];
-  return YES;
 }
+@end
+
+@interface POCAppDelegate : NSObject<UIApplicationDelegate>
+@end
+@implementation POCAppDelegate
+- (UISceneConfiguration *) application:(UIApplication *) application 
+configurationForConnectingSceneSession:(UISceneSession *) connectingSceneSession 
+                               options:(UISceneConnectionOptions *) options
+{
+  // Kinda the only way after iOS 26
+
+  UISceneConfiguration * res = [[UISceneConfiguration alloc] initWithName:@"poc"
+                                                              sessionRole:connectingSceneSession.role];
+  res.sceneClass = [UIWindowScene class];
+  res.delegateClass = [POCWindowSceneDelegate class];
+  return res;
+}
+
 @end
 
 // In theory, just a 'int main() {}' would already be installable on an iOS
